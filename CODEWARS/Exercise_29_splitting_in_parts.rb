609bc8fa78704b3a762af8d5bb44a0d9dc2372e5
Aga <a.kolia.grudzien@gmail.com>
2017-06-15 14:58:36 +0200

    Exercising: two solutions

--- CODEWARS/Exercise_29_splitting_in_parts.rb
+++ CODEWARS/Exercise_29_splitting_in_parts.rb
@@ -1,5 +1,19 @@
 # https://www.codewars.com/kata/split-in-parts/train/ruby
+
+# Option 1 with regular expression:
 def split_in_parts (s, part_length = 3)
-  splitted = s.scan(/. {1, #{part_length}}/)
+  splitted = s.scan(/.{1,#{part_length}}/)
   splitted.join(" ")
+end
+
+
+# Option 2 with a loop:
+def split_by_three(s, part_length = 3)
+    splitted = []
+    i = 0
+      while i < s.size do
+        splitted << s.slice(i, part_length)
+        i = i + part_length
+      end
+      splitted.join(" ")
 end
