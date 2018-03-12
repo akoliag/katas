069f972e55119a64b48ece30291c7f288f95637c
Aga <a.kolia.grudzien@gmail.com>
2018-03-12 17:26:52 +0100

    Exercising

---
+++ CODEWARS/Exercise_76_secret_message.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/jennys-secret-message/train/javascript
+function greet(name) {
+  if (name === "Johnny") {
+    return "Hello, my love!";
+  } else {
+    return `Hello, ${name}!`
+  }
+}
