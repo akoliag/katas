cee6ed3ad1a6b2832cd13ce74425267942677a9f
Aga <a.kolia.grudzien@gmail.com>
2018-03-19 16:50:48 +0100

    Add another option

--- CODEWARS/Exercise_81_find_the_needle.js
+++ CODEWARS/Exercise_81_find_the_needle.js
@@ -1,4 +1,6 @@
 //https://www.codewars.com/kata/a-needle-in-the-haystack
+
+//Option 1:
 function findNeedle(haystack) {
   for (var i = 0; i < haystack.length; i++){
     if (haystack[i] === "needle"){
@@ -7,3 +9,8 @@ function findNeedle(haystack) {
   }
 }
 console.log(findNeedle(['hay', 'junk', 'needle', 'moreJunk']));
+
+//Option 2:
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
