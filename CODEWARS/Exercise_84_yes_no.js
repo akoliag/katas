cf3c76ca1c177bb1442d2a7582faf46a10b1a19d
Aga <a.kolia.grudzien@gmail.com>
2018-03-20 21:17:40 +0100

    Exercising

---
+++ CODEWARS/Exercise_84_yes_no.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/javascript
+
+function boolToWord( bool ){
+  return bool === true ? "Yes" : "No";
+}
+
+console.log(boolToWord(false));
