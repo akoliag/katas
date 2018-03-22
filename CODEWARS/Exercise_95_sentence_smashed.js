d0ae6b78b742cf2656cce8bced7f7744c74b5267
Aga <a.kolia.grudzien@gmail.com>
2018-03-22 19:59:00 +0100

    Exercising

---
+++ CODEWARS/Exercise_95_sentence_smashed.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/sentence-smash/train/javascript
+function smash (words) {
+    return words.join(' ');
+};
+
+console.log(smash(['Take', 'one', 'and', 'then', 'another', 'one.']));
