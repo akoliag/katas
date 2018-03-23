4d1740b92822901510c2e98fd3b2ef07b2024ab5
Aga <a.kolia.grudzien@gmail.com>
2018-03-23 20:23:03 +0100

    Exercising

---
+++ CODEWARS/Exercise_75_volume_of_cuboid.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/volume-of-a-cuboid/train/javascript
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
