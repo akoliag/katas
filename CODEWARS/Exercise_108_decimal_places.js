569cca725ba327271be0f7b72102e85f4ea3eb85
Aga <a.kolia.grudzien@gmail.com>
2018-03-26 19:25:04 +0200

    Add other options

--- CODEWARS/Exercise_108_decimal_places.js
+++ CODEWARS/Exercise_108_decimal_places.js
@@ -1,7 +1,16 @@
 //https://www.codewars.com/kata/formatting-decimal-places-number-0
 
+//Option 1:
 function twoDecimalPlaces(n) {
   return Number(n.toFixed(2));
 }
 
 console.log(twoDecimalPlaces(3.33498));
+
+//Option 2:
+const twoDecimalPlaces = (n) => Math.round(n * 100) / 100;
+
+//Option 3:
+function twoDecimalPlaces(n) {
+  return parseFloat(n.toFixed(2));
+}
