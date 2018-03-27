aad529526e25a0c01fc662b8af2b9f5ced1b4365
Aga <a.kolia.grudzien@gmail.com>
2018-03-27 19:35:24 +0200

    Exercising

---
+++ CODEWARS/Exercise_107_welcomeToTheCity.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/welcome-to-the-city/train/javascript
+
+function sayHello( name, city, state ) {
+return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+console.log((['John', 'Smith'], 'Los Angeles', 'California'));
