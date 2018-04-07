89665ac0e462727334d90c26257cd36cc6a8f9f6
Aga <a.kolia.grudzien@gmail.com>
2018-04-07 16:34:32 +0200

    Exercising

---
+++ CODEWARS/Exercise_123_ES6_string_addition.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/es6-string-addition/train/javascript
+
+function joinStrings(string1, string2){
+   return `${string1} ${string2}`;
+}
+
+console.log(joinStrings("Aga", "G"));
