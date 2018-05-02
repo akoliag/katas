ca6ee929b9dcd562ba29d59df1b0dd74ff81e1e0
Aga <a.kolia.grudzien@gmail.com>
2018-05-02 20:56:58 +0200

    2nd solution

--- CODEWARS/IB_8_capitalization.js
+++ CODEWARS/IB_8_capitalization.js
@@ -13,3 +13,19 @@ function capitalize(str) {
 //providing only the first arg with slice() we take eth from that elem to the
 //very end
 console.log(capitalize('a short sentence'));
+
+//option 2:
+function capitalize1(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+//we look to the left of the current character and chaeck if this is a space
+console.log(capitalize1('you are a big boy'));
