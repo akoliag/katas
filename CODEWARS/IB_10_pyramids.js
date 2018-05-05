c8dd1e050c8a06933bf1afbb9f111ce914189d85
Aga <a.kolia.grudzien@gmail.com>
2018-05-05 22:59:04 +0200

    add second solution

--- CODEWARS/IB_10_pyramids.js
+++ CODEWARS/IB_10_pyramids.js
@@ -11,7 +11,7 @@ function pyramid(n) {
 
     for (let column = 0; column < n * 2 - 1; column++) {
       if (midpoint - row <= column && midpoint + row >= column) {
-          level += '#';
+        level += '#';
       } else {
         level += ' ';
       }
@@ -21,3 +21,27 @@ function pyramid(n) {
 }
 
 console.log(pyramid(5));
+
+
+//option 2 - recursive solution:
+function pyramid1(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === n * 2 - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
+
+console.log(pyramid1(8));
