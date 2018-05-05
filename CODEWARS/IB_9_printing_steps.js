c9291cfa59b3a5f19b22c526be0d1b1f5589ceab
Aga <a.kolia.grudzien@gmail.com>
2018-05-05 19:53:28 +0200

    add second solution

--- CODEWARS/IB_9_printing_steps.js
+++ CODEWARS/IB_9_printing_steps.js
@@ -25,3 +25,25 @@ function steps(n) {
 }
 
 console.log(steps(7));
+
+//option 2 - recursive solution:
+
+function steps1(n, row = 0, stair = ''){
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
+
+console.log(steps1(4));
