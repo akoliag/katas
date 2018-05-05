00241f1ada6dd95924faefab7af2347458d010f3
Aga <a.kolia.grudzien@gmail.com>
2018-05-05 23:38:33 +0200

    add second solution

--- CODEWARS/IB_11_find_a_vowel.js
+++ CODEWARS/IB_11_find_a_vowel.js
@@ -13,3 +13,17 @@ function vowelsCount(str) {
 }
 
 console.log(vowelsCount('Monika'));
+
+//option 2:
+function vowelsCount1(str) {
+  let count = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(vowelsCount1('Agnieszka'));
