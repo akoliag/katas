92f67d06619f6a632c6e8c3fd20f7da83fac95cb
Aga <a.kolia.grudzien@gmail.com>
2018-06-01 21:15:06 +0200

    data structure intro

---
+++ CODEWARS/IB_16_Data_Structure.js
@@ -0,0 +1,11 @@
+/*Data Structure - ways of organizing information with optimal 'runtime
+complexity' for adding or editing or removing records.
+
+a queue may be thought of as a conatainer of source where records or pieces of
+data enter on one end of this container and then exit on the other. A queue is
+like waiting in line to do sth.
+
+Enqueuing (adding) - the process of adding a record into a queue
+Dequeuing (removing) - taking sth out the queue
+
+Queue follows First-in-First out principle (F-I-F-O).
