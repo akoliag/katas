ac82e3a8072c10a0bfef8b7844d24031179aa97f
Aga <a.kolia.grudzien@gmail.com>
2018-06-02 13:21:58 +0200

    how to weave part II

---
+++ CODEWARS/IB_18_index.js
@@ -0,0 +1,17 @@
+
+const Queue = require('./queue');
+
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+
+  while(sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()){
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+  return q;
+}
